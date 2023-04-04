20b7ce855df9ae077ed3d370d8a94adec676b339
gvnfcosta <gvnfcosta@gmail.com>
2023-04-04 13:20:04 -0300

    teste

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'every_widgets/about_dialog.dart';
 import 'every_widgets/about_list_tile.dart';
+import 'every_widgets/absorb_pointer.dart';
+import 'every_widgets/alert_dialog.dart';
+import 'every_widgets/align.dart';
+import 'every_widgets/animated_align.dart';
+import 'every_widgets/animated_builder.dart';
+import 'every_widgets/animated_container.dart';
+import 'every_widgets/animated_cross_fade.dart';
 import 'every_widgets/sliver_fixed_extent_list.dart';
 import 'every_widgets/stepper.dart';
 import 'every_widgets/switch_list_tile.dart';
 import 'every_widgets/tab_bar.dart';
+import 'every_widgets/tab_page_selector.dart';
+import 'every_widgets/table.dart';
 
 const Color darkBlue = Color(0xFF12202F);
 
@@ -24,7 +33,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(title: const Text('Flutter Mapp')),
         //! Just change the Widget001 for the Widget you want.
-        body: const Widget197(),
+        body: const Widget009(),
       ),
     );
   }
